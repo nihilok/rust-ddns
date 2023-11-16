7d432c4cfcd434a129769834d213d146924f08b6
nihilok <mjfullstack@gmail.com>
2023-11-16 22:27:43 +0000

    Enhance install.sh with additional logging and safeguard
    The improvements to `install.sh` include additional output messages to inform the user of the current status and steps being performed. Further, a safeguard has been introduced to avoid the unnecessary creation of symbolic links if they already exist.

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ RUN_SCRIPT='ddnsd'
 CURRENT_DIR=$(pwd)
 
 if [[ -z $RUST_DDNS_INTERVAL ]]; then
-RUST_DDNS_INTERVAL=5min
+    echo "RUST_DDNS_INTERVAL is not set, defaulting to 5min."
+    RUST_DDNS_INTERVAL=5min
 fi
 
 if [[ -f 'install.sh' ]] && [[ -f $RUN_SCRIPT ]]; then
-echo "\
+    echo "Creating rust-ddns systemd timer and service..."
+
+    echo "\
 [Unit]
 Description=Runs rust-ddns every 5 minutes
 
@@ -22,7 +25,8 @@ Unit=rust-ddns.service
 [Install]
 WantedBy=multi-user.target
 " | sudo tee /etc/systemd/system/rust-ddns.timer >/dev/null
-echo "\
+
+    echo "\
 [Unit]
 Description=Run rust-ddns once
 
@@ -34,17 +38,32 @@ Environment=HOME=$HOME
 Environment=PATH=$HOME/.local/bin:$PATH
 " | sudo tee /etc/systemd/system/rust-ddns.service >/dev/null
 
-cargo build --release
+    echo "Successfully created rust-ddns timer and service."
 
-mkdir -p $HOME/.local/bin
+    echo "Compiling Rust application..."
+    cargo build --release
+    echo "Build complete."
 
-SYMBOLIC_LINK_PATH="$HOME/.local/bin/rust-ddns"
-if [[ ! -f $SYMBOLIC_LINK_PATH ]]; then
-    ln -s $CURRENT_DIR/target/release/rust-ddns $SYMBOLIC_LINK_PATH
+    mkdir -p $HOME/.local/bin
+    SYMBOLIC_LINK_PATH="$HOME/.local/bin/rust-ddns"
+    if [[ ! -f $SYMBOLIC_LINK_PATH ]]; then
+        echo "Creating symbolic link to binary in local binaries folder..."
+        ln -s $CURRENT_DIR/target/release/rust-ddns $SYMBOLIC_LINK_PATH
+    else
+        echo "Symbolic link already exists. Skipping creation."
+    fi
+
+    echo "Reloading systemd manager configuration..."
+    sudo systemctl daemon-reload
+else
+    echo "Required files not found. Make sure you are running the script from within the source directory" && exit 1
 fi
 
-sudo systemctl daemon-reload
+echo "Installation complete!"
 
-else
-echo "Required files not found. Make sure you are running the script from within the source directory" && exit 1
+if ! echo $PATH | grep -q "$HOME/.local/bin"; then
+    echo "Warning: Your PATH does not include the local binaries folder."
+    echo "To add it to your PATH, append the following line to your shell's configuration file (usually either ~/.bashrc or ~/.zshrc):"
+    echo "export PATH=\$PATH:\$HOME/.local/bin"
+    echo "Then, reload the configuration with the command 'source ~/.bashrc' or 'source ~/.zshrc'."
 fi
